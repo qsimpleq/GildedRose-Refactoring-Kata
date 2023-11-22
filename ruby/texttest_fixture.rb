2f88da4fe561b31f6bbbe4ffa004170b350b8f9c
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-11-22 16:39:12 +0500

    Remove UselessAssignments

--- ruby/texttest_fixture.rb
+++ ruby/texttest_fixture.rb
@@ -1,26 +1,24 @@
 #!/usr/bin/ruby -w
 # frozen_string_literal: true
 
-require File.join(File.dirname(__FILE__), 'gilded_rose')
+require File.join(File.dirname(__FILE__), "gilded_rose")
 
 puts "OMGHAI!"
 items = [
-  Item.new(name="+5 Dexterity Vest", sell_in=10, quality=20),
-  Item.new(name="Aged Brie", sell_in=2, quality=0),
-  Item.new(name="Elixir of the Mongoose", sell_in=5, quality=7),
-  Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
-  Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
-  Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-  Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-  Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+  Item.new("+5 Dexterity Vest", 10, 20),
+  Item.new("Aged Brie", 2, 0),
+  Item.new("Elixir of the Mongoose", 5, 7),
+  Item.new("Sulfuras, Hand of Ragnaros", 0, 80),
+  Item.new("Sulfuras, Hand of Ragnaros", -1, 80),
+  Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+  Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+  Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 49),
   # This Conjured item does not work properly yet
-  Item.new(name="Conjured Mana Cake", sell_in=3, quality=6), # <-- :O
+  Item.new("Conjured Mana Cake", 3, 6) # <-- :O
 ]
 
 days = 2
-if ARGV.size > 0
-  days = ARGV[0].to_i + 1
-end
+days = ARGV[0].to_i + 1 if ARGV.size.positive?
 
 gilded_rose = GildedRose.new items
 (0...days).each do |day|
