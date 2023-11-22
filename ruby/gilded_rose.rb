d092561f20807dae55606161b500f863616e25b6
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-11-22 16:35:10 +0500

    Move Item to separate file

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'item'
+
 class GildedRose
 
   def initialize(items)
@@ -52,19 +54,5 @@ class GildedRose
         end
       end
     end
-  end
-end
-
-class Item
-  attr_accessor :name, :sell_in, :quality
-
-  def initialize(name, sell_in, quality)
-    @name = name
-    @sell_in = sell_in
-    @quality = quality
-  end
-
-  def to_s()
-    "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
