bd3e998db657363bb858956c4c0e80cf9850ed57
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-11-23 14:18:17 +0500

    Add handle_quality and handle_expired_item

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -32,6 +32,12 @@ class GildedRose
       max_quality: DEFAULT_MAX_QUALITY
     }
   }.freeze
+  AGED_BRIE = GOODS[:aged_brie][:name]
+  BACKSTAGE_PASS = GOODS[:tafkal80etc_concert_pass][:name]
+  SULFURAS = GOODS[:sulfuras][:name]
+  DEFAULT_QUALITY = 1
+  MAX_QUALITY = DEFAULT_MAX_QUALITY
+  MIN_QUALITY = 0
 
   def initialize(items)
     @items = items
@@ -39,37 +45,42 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
-      if (item.name != GOODS[:aged_brie][:name]) && (item.name != GOODS[:tafkal80etc_concert_pass][:name])
-        item.quality = item.quality - 1 if item.quality.positive? && (item.name != GOODS[:sulfuras][:name])
-      elsif item.quality < DEFAULT_MAX_QUALITY
-        item.quality = item.quality + 1
-        if item.name == GOODS[:tafkal80etc_concert_pass][:name]
-          item.quality = item.quality + 1 if item.sell_in < 11 && (item.quality < DEFAULT_MAX_QUALITY)
-          item.quality = item.quality + 1 if item.sell_in < 6 && (item.quality < DEFAULT_MAX_QUALITY)
-        end
-      end
-
+      handle_quality(item)
       decrement_sell_in(item)
 
-      if item.sell_in.negative?
-        if item.name != GOODS[:aged_brie][:name]
-          if item.name != GOODS[:tafkal80etc_concert_pass][:name]
-            item.quality = item.quality - 1 if item.quality.positive? && (item.name != GOODS[:sulfuras][:name])
-          else
-            item.quality = 0
-          end
-        elsif item.quality < DEFAULT_MAX_QUALITY
-          item.quality = item.quality + 1
-        end
-      end
+      handle_expired_item(item) if item.sell_in.negative?
     end
   end
 
   private
 
+  def handle_quality(item)
+    if (item.name != GOODS[:aged_brie][:name]) && (item.name != GOODS[:tafkal80etc_concert_pass][:name])
+      item.quality = item.quality - 1 if item.quality.positive? && (item.name != GOODS[:sulfuras][:name])
+    elsif item.quality < DEFAULT_MAX_QUALITY
+      item.quality = item.quality + 1
+      if item.name == GOODS[:tafkal80etc_concert_pass][:name]
+        item.quality = item.quality + 1 if item.sell_in < 11 && (item.quality < DEFAULT_MAX_QUALITY)
+        item.quality = item.quality + 1 if item.sell_in < 6 && (item.quality < DEFAULT_MAX_QUALITY)
+      end
+    end
+  end
+
   def decrement_sell_in(item)
     return unless item.name != GOODS[:sulfuras][:name]
 
     item.sell_in = item.sell_in - 1
+  end
+
+  def handle_expired_item(item)
+    if item.name != GOODS[:aged_brie][:name]
+      if item.name != GOODS[:tafkal80etc_concert_pass][:name]
+        item.quality = item.quality - 1 if item.quality.positive? && (item.name != GOODS[:sulfuras][:name])
+      else
+        item.quality = 0
+      end
+    elsif item.quality < DEFAULT_MAX_QUALITY
+      item.quality = item.quality + 1
+    end
   end
 end
